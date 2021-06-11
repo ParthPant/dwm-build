a471f639dc4724dd2b749f645373fbcf789c7466
Parth Pant <parthpant4@gmail.com>
2021-06-11 23:50:01 +0530

    autostart scripts

---
+++ .dwm/autostart.sh
@@ -0,0 +1,17 @@
+compton --backend=glx --paint-on-overlay --vsync &
+
+dunst &
+mailspring -b &
+blueman-applet &
+nm-applet &
+volumeicon &
+nitrogen --restore &
+cbatticon -i symbolic &
+dwmblocks &
+
+#trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --transparent true --alpha 0 --tint 0x282c34 --height 22 &
+
+#while true; do
+    #xsetroot -name "$(date +"%a %d %b %I:%M")"
+    #sleep 1m
+#done &
